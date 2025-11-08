1112363c797b58ba0e9ff05ab72789601a6e9bc0
szymonf123 <fortunaszymon@wp.pl>
2025-11-08 15:46:18 +0100

    Add buttons

--- zad1/zad1/ViewController.swift
+++ zad1/zad1/ViewController.swift
@@ -13,7 +13,54 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBOutlet weak var label_result: UILabel!
+    @IBAction func button0(_ sender: Any) {
+        print("0")
+    }
+    @IBAction func button1(_ sender: Any) {
+        print("1")
+    }
+    @IBAction func button2(_ sender: Any) {
+        print("2")
+    }
+    @IBAction func button3(_ sender: Any) {
+        print("3")
+    }
+    @IBAction func button4(_ sender: Any) {
+        print("4")
+    }
+    @IBAction func button5(_ sender: Any) {
+        print("5")
+    }
+    @IBAction func button6(_ sender: Any) {
+        print("6")
+    }
+    @IBAction func button7(_ sender: Any) {
+        print("7")
+    }
+    @IBAction func button8(_ sender: Any) {
+        print("8")
+    }
+    @IBAction func button9(_ sender: Any) {
+        print("9")
+    }
+    @IBAction func button_eq(_ sender: Any) {
+        print("=")
+    }
+    @IBAction func buttonC(_ sender: Any) {
+        print("clear")
+    }
+    @IBAction func button_add(_ sender: Any) {
+        print("+")
+    }
+    @IBAction func button_subtract(_ sender: Any) {
+        print("-")
+    }
+    @IBAction func button_multiply(_ sender: Any) {
+        print("*")
+    }
+    @IBAction func button_divide(_ sender: Any) {
+        print("/")
+    }
 }
 
