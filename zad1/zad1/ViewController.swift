03f49c9c3f4b43bb279438029d8d7bd108364803
szymonf123 <fortunaszymon@wp.pl>
2025-11-08 14:49:13 +0100

    Create empty app for task 1

---
+++ zad1/zad1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zad1
+//
+//  Created by user279406 on 11/8/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
