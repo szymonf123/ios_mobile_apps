2ee1597bd4aeaa9228a1cd737b559080f44068cb
szymonf123 <fortunaszymon@wp.pl>
2025-11-15 12:50:34 +0100

    Add summing, result and clearing

--- zad1/zad1/ViewController.swift
+++ zad1/zad1/ViewController.swift
@@ -8,6 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var a: Int = 0
+    var b: Int = 0
+    var operation: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -15,43 +19,49 @@ class ViewController: UIViewController {
     }
     @IBOutlet weak var label_result: UILabel!
     @IBAction func button0(_ sender: Any) {
-        print("0")
+        label_result.text = (label_result.text ?? "") + "0"
     }
     @IBAction func button1(_ sender: Any) {
-        print("1")
+        label_result.text = (label_result.text ?? "") + "1"
     }
     @IBAction func button2(_ sender: Any) {
-        print("2")
+        label_result.text = (label_result.text ?? "") + "2"
     }
     @IBAction func button3(_ sender: Any) {
-        print("3")
+        label_result.text = (label_result.text ?? "") + "3"
     }
     @IBAction func button4(_ sender: Any) {
-        print("4")
+        label_result.text = (label_result.text ?? "") + "4"
     }
     @IBAction func button5(_ sender: Any) {
-        print("5")
+        label_result.text = (label_result.text ?? "") + "5"
     }
     @IBAction func button6(_ sender: Any) {
-        print("6")
+        label_result.text = (label_result.text ?? "") + "6"
     }
     @IBAction func button7(_ sender: Any) {
-        print("7")
+        label_result.text = (label_result.text ?? "") + "7"
     }
     @IBAction func button8(_ sender: Any) {
-        print("8")
+        label_result.text = (label_result.text ?? "") + "8"
     }
     @IBAction func button9(_ sender: Any) {
-        print("9")
+        label_result.text = (label_result.text ?? "") + "9"
     }
     @IBAction func button_eq(_ sender: Any) {
-        print("=")
+        b = Int(label_result.text ?? "") ?? 0
+        if (operation == "+"){
+            label_result.text = String(a + b)
+        }
     }
     @IBAction func buttonC(_ sender: Any) {
-        print("clear")
+        label_result.text = ""
+        operation = ""
     }
     @IBAction func button_add(_ sender: Any) {
-        print("+")
+        a = Int(label_result.text ?? "") ?? 0
+        label_result.text = ""
+        operation = "+"
     }
     @IBAction func button_subtract(_ sender: Any) {
         print("-")
