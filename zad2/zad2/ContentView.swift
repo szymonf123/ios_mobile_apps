18c6d6502c754fec3f36fc620a4fab89d4a27556
szymonf123 <fortunaszymon@wp.pl>
2025-11-29 14:47:59 +0100

    Task 2 4.0

--- zad2/zad2/ContentView.swift
+++ zad2/zad2/ContentView.swift
@@ -7,24 +7,34 @@
 
 import SwiftUI
 
+struct TaskItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var done: Bool
+}
 struct ContentView: View {
+    @State private var tasks = [
+            TaskItem(name: "Iść do okulisty zbadać tęczówkę", done: false),
+            TaskItem(name: "Zrobić odcisk palca do wizy", done: false),
+            TaskItem(name: "Iść na wykład o starożytnych Chinach", done: false),
+            TaskItem(name: "Przygotować referat na za tydzień", done: false)
+        ]
     var body: some View {
         VStack {
             
-            Text("Lista rozmaitych zadan")
+            Text("Lista rozmaitych zadań")
             List {
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    Label("Isc do okulisty zbadac teczowke", systemImage: "t.circle")
+                ForEach(tasks.indices, id: \.self) { index in
+                    HStack {
+                        Toggle(isOn: $tasks[index].done) {
+                            Label(tasks[index].name, systemImage: "t.circle")
+                            }
+                        Button("X") {
+                            tasks.remove(at: index)
+                        }
+                    }
                 }
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    Label("Zrobic odcisk palca do wizy", systemImage: "t.circle")
-                }
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    Label("Isc na wyklad o starozytnych Chinach", systemImage: "t.circle")
-                }
-                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
-                    Label("Przygotowac referat na za tydzien", systemImage: "t.circle")
-                }            }
+            }
         }
         .padding()
     }
