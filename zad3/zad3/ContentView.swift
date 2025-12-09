703388ff9dccf31700cd5028b9daa2c11641025e
szymonf123 <fortunaszymon@wp.pl>
2025-12-09 19:53:04 +0100

    Task 3 4.5 and 5.0

--- zad3/zad3/ContentView.swift
+++ zad3/zad3/ContentView.swift
@@ -8,11 +8,16 @@
 import SwiftUI
 import CoreData
 
+let screenHeight = UIScreen.main.bounds.height
+
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var context
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Product.name, ascending: true)]
         ) private var products: FetchedResults<Product>
+    
+    @State private var cart = [Product]()
+    
     var body: some View {
         NavigationStack {
             VStack {
@@ -27,10 +32,24 @@ struct ContentView: View {
                                     Text(String(format: "%.2f PLN", product.price)).foregroundStyle(.blue)
                                 }
                                 Text(product.category?.name ?? "-").foregroundStyle(.gray)
+                                Button("Dodaj do koszyka") {
+                                    cart.append(product)
+                                }
+                                .buttonStyle(.borderless)
                             }
                         }
                     }
                 }
+                if (cart.count > 0){
+                    Text("Koszyk")
+                    List {
+                        ForEach(Array(cart)){ item in
+                            Text(item.name ?? "-")
+                        }
+                    }
+                    .frame(height: screenHeight * 0.25)
+                }
+                //.frame(height: screenHeight * 0.25 / CGFloat(max(cart.count, 1)))
             }
         }
     }
